db2c248c862ab51417ccd35dd24dc85ecd472b7f
Danbob-Airsoft <43638476+Danbob-Airsoft@users.noreply.github.com>
2019-03-09 12:44:46 +0000

    Presentation version
    Version of the game shown and used in the first presentation

---
+++ Daniel Bailey/Game Assets/Test/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Movement Variables
+    public Rigidbody2D RigidBody;
+    public float Movespeed;
+
+    void FixedUpdate()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            RigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * Movespeed;
+        }
+        else
+        {
+            RigidBody.velocity = Vector2.zero;
+        }
+
+    }
+}
---
+++ Daniel Bailey/Game Assets/Test/Assets/Scripts/Player/TrapScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (this.gameObject.tag == ("UpgradedTrap"))
+        {
+            if (collision.tag == ("Enemy"))
+            {
+                Destroy(collision.gameObject);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
